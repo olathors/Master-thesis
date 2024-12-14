277df6b5606b20c911b1db387cda5d60c55f104f
Ola Thorsen <olathors@gmail.com>
2024-12-14 11:02:28 -0300

    Wrote script for executing preprocessing

---
+++ Data_Preprocessing/execute_preprocessing.py
@@ -0,0 +1,27 @@
+from mri_preprocess import preprocess_image
+import ants
+import numpy as np
+import time
+
+def main():
+
+    data = np.load("/Users/olath/Documents/GitHub/Master-thesis/Data_Experimentation/images_with_paths.npy",allow_pickle='TRUE').item()
+
+    atlas_image = ants.image_read('//Users/olath/Downloads/mni_icbm152_nlin_sym_09a_nifti/mni_icbm152_nlin_sym_09a/mni_icbm152_t1_tal_nlin_sym_09a.nii')
+    counter = 0
+    images_left = len(data)
+    global_time = time.time()
+
+    for key in data:
+        local_time = time.time()
+        preprocess_image(data[key], '/Users/olath/Documents/GitHub/Master-thesis/Data_Preprocessing/test_data/', 'I'+str(key), atlas_image)
+        counter += 1
+        images_left -= 1
+        local_time_spent = (time.time() - local_time)
+        global_time_spent = (time.time() - global_time) / 60
+        print ('Processed image nr:',counter,'- id:',key, 'in %.2f' % local_time_spent ,"seconds.")
+        print('There are', images_left, 'images left to preprocess.')
+        print('Total time spent is %.2f' % global_time_spent, "minutes.")
+        print('\n')
+
+main()

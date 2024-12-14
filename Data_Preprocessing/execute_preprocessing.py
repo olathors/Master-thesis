a90839a75f6144b9a2fd4bd6c0d66b04ffb15be8
Ola Thorsen <olathors@gmail.com>
2024-12-14 19:49:22 -0300

    Finished code for preprocessing

--- Data_Preprocessing/execute_preprocessing.py
+++ Data_Preprocessing/execute_preprocessing.py
@@ -2,19 +2,29 @@ from mri_preprocess import preprocess_image
 import ants
 import numpy as np
 import time
+import zipfile
+import shutil
 
-def main():
+def main(in_data, images):
 
-    data = np.load("/Users/olath/Documents/GitHub/Master-thesis/Data_Experimentation/images_with_paths.npy",allow_pickle='TRUE').item()
+    data = np.load(images,allow_pickle='TRUE').item()
 
     atlas_image = ants.image_read('//Users/olath/Downloads/mni_icbm152_nlin_sym_09a_nifti/mni_icbm152_nlin_sym_09a/mni_icbm152_t1_tal_nlin_sym_09a.nii')
     counter = 0
     images_left = len(data)
     global_time = time.time()
 
+    archive = zipfile.ZipFile(in_data)
+
     for key in data:
         local_time = time.time()
-        preprocess_image(data[key], '/Users/olath/Documents/GitHub/Master-thesis/Data_Preprocessing/test_data/', 'I'+str(key), atlas_image)
+
+        for file in archive.namelist():
+            if file.endswith('I'+key+'.dcm'):
+                archive.extract(file, 'temp_folder')
+
+
+        preprocess_image('temp_folder', '/Users/olath/Documents/GitHub/Master-thesis/Data_Preprocessing/test_data/', 'I'+str(key), atlas_image)
         counter += 1
         images_left -= 1
         local_time_spent = (time.time() - local_time)
@@ -24,4 +34,8 @@ def main():
         print('Total time spent is %.2f' % global_time_spent, "minutes.")
         print('\n')
 
-main()
+        shutil.rmtree('temp_folder')
+
+
+
+main('/Volumes/Extreme SSD/Download/Download_collection1.zip', "/Users/olath/Documents/GitHub/Master-thesis/Data_Experimentation/images_with_paths_file_1.npy")
